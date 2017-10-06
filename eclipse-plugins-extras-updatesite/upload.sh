25154d3dbb0f3376b20039e059256f1964f3d86e
gayanper <gayanper@gmail.com>
2017-10-06 20:46:04 +0530

    upload file updated

--- eclipse-plugins-extras-updatesite/upload.sh
+++ eclipse-plugins-extras-updatesite/upload.sh
@@ -4,4 +4,5 @@ API_KEY=$1
 ARCHIVE=$2
 VERSION=$3
 
-curl -X PUT -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-plugins-extras/$VERSION/eclipse-plugins-extras/$VERSION/archive-site.zip?explode=1
+curl -T $ARCHIVE -ugayanper:$API_KEY https://api.bintray.com/content/gayanper/p2/eclipse-plugins-extras/$VERSION/eclipse-plugins-extras/$VERSION/archive-site.zip?explode=1
+
