08f8e1eaef051f564225f100c71f4f596918b9b1
Gayan Perera <gayanper@gmail.com>
2017-11-28 22:33:14 +0530

    Improvements with new open nested action
    Added new action to open nested project and controlled visibility of
    menus based on project state.

---
+++ eclipse-plugins-extras-core/src/org/gap/eclipse/plugins/extras/core/project/OpenNestedProjectsHandler.java
@@ -0,0 +1,52 @@
+package org.gap.eclipse.plugins.extras.core.project;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.function.Consumer;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.ISelectionService;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.progress.IProgressService;
+import org.gap.eclipse.plugins.extras.core.PluginActivator;
+
+public class OpenNestedProjectsHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		final IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+		final IProgressService progressService = window.getWorkbench().getProgressService();
+		
+		final ISelectionService selectionService = window.getSelectionService();
+		final IStructuredSelection selection = ((IStructuredSelection) selectionService.getSelection());
+		final IProject project = ((IProject) selection.getFirstElement());
+
+		try {
+			progressService.run(true, false, pm -> openAllProjects(project, pm));
+		} catch (InvocationTargetException | InterruptedException e) {
+			PluginActivator.getDefault().log(Status.INFO, "Stopped project closing job.", e);
+		}
+		return null;
+	}
+
+	private void openAllProjects(IProject project, IProgressMonitor pm) {
+		final Consumer<IProject> projectConsumer = p -> {
+			if (!p.isOpen()) {
+				try {
+					p.open(pm);
+				} catch (CoreException e) {
+					PluginActivator.getDefault().log(Status.ERROR, "Error opening projects.", e);
+				}
+			}
+		};
+		projectConsumer.accept(project);
+		NestedProjectWalker.walkerFor(project).forEach(projectConsumer);
+	}
+}
