fd4764469231a6211ad756f94fdc7a03de3122fe
gayanper <gayanper@gmail.com>
2019-07-18 23:00:24 +0530

    Fix #4: removed recursion to avoid SOFE with large object graphs

--- eclipse-plugins-extras-core/src/org/gap/eclipse/plugins/extras/core/debug/JsonGenerator.java
+++ eclipse-plugins-extras-core/src/org/gap/eclipse/plugins/extras/core/debug/JsonGenerator.java
@@ -1,5 +1,8 @@
 package org.gap.eclipse.plugins.extras.core.debug;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
@@ -14,36 +17,57 @@ import org.eclipse.jdt.debug.core.IJavaValue;
 import org.eclipse.jdt.debug.core.IJavaVariable;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterators;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class JsonGenerator {
 	private Set<String> terminalTypes = ImmutableSet.of("java.lang.String");
-	
+
 	public JsonObject construct(IJavaVariable variable) throws CoreException {
 		JsonObject jsonObject = new JsonObject();
-		populateJson(variable, jsonObject);
+		dumpToJson(Iterators.forArray(variable), jsonObject);
 		return jsonObject;
 	}
 
-	
-	private void populateJson(IJavaVariable variable, JsonObject jsonObject) throws CoreException {
-		final IValue value = logicalValue(variable.getValue());
-		if(isTerminalType(value)) {
-			jsonObject.addProperty(variable.getName(), value.getValueString());
-		} else if(isArray(value)) {
-			final IJavaValue[] values = ((IJavaArray) value).getValues();
-			jsonObject.add(variable.getName(), createJsonArray(values));
-		} else {
-			final JsonObject innerObject = new JsonObject();
-			jsonObject.add(variable.getName(), innerObject);
-			for (IVariable subVariable : value.getVariables()) {
-				populateJson((IJavaVariable) subVariable, innerObject);
+	private void dumpToJson(final Iterator<IVariable> variables, final JsonObject json) throws CoreException {
+		final Deque<IterationEntry> entryStack = new ArrayDeque<>();
+		Iterator<IVariable> runningIterator = variables;
+		JsonObject runningJson = json;
+
+		while (runningIterator.hasNext()) {
+			IJavaVariable variable = (IJavaVariable) runningIterator.next();
+			IValue value = logicalValue(variable.getValue());
+
+			if (isTerminalType(value) || isArray(value)) {
+				if (isTerminalType(value)) {
+					runningJson.addProperty(variable.getName(), value.getValueString());
+				} else {
+					final IJavaValue[] values = ((IJavaArray) value).getValues();
+					runningJson.add(variable.getName(), createJsonArray(values));
+				}
+
+				if (!runningIterator.hasNext()) {
+					if (entryStack.isEmpty()) {
+						break;
+					}
+
+					// pop the stack
+					IterationEntry entry = entryStack.pop();
+					runningIterator = entry.variables;
+					runningJson = entry.json;
+				}
+			} else {
+				// complex variable push to stack and continue
+				entryStack.push(IterationEntry.entry(runningIterator, runningJson));
+				runningIterator = Iterators.forArray(value.getVariables());
+				runningJson = new JsonObject();
+				entryStack.peek().json.add(variable.getName(), runningJson);
 			}
 		}
 	}
-	
+
 	private IValue logicalValue(IValue value) throws CoreException {
 		ILogicalStructureType[] types = DebugPlugin.getLogicalStructureTypes(value);
 		if (types.length > 0) {
@@ -62,14 +86,13 @@ public class JsonGenerator {
 			if (isTerminalType(value)) {
 				array.add(value.getValueString());
 			} else if (isArray(value)) {
-				array.add(createJsonArray(((IJavaArray)value).getValues()));
+				array.add(createJsonArray(((IJavaArray) value).getValues()));
 			} else {
 				JsonObject innerObject = new JsonObject();
 				array.add(innerObject);
 				for (IVariable variable : value.getVariables()) {
-					populateJson((IJavaVariable) variable, innerObject);
+					dumpToJson(Iterators.forArray((IJavaVariable) variable), innerObject);
 				}
-
 			}
 		}
 
@@ -77,14 +100,30 @@ public class JsonGenerator {
 	}
 
 	private boolean isTerminalType(IValue value) throws DebugException {
-		if(value instanceof IJavaPrimitiveValue) {
+		if (value instanceof IJavaPrimitiveValue) {
 			return true;
-		}else {
+		} else {
 			return terminalTypes.contains(value.getReferenceTypeName());
 		}
 	}
-	
+
 	private boolean isArray(IValue value) {
 		return value instanceof IJavaArray;
+	}
+
+	public static class IterationEntry {
+		public Iterator<IVariable> variables;
+
+		public JsonObject json;
+
+		private IterationEntry() {
+		}
+
+		public static IterationEntry entry(Iterator<IVariable> variables, JsonObject json) {
+			IterationEntry entry = new IterationEntry();
+			entry.variables = variables;
+			entry.json = json;
+			return entry;
+		}
 	}
 }
